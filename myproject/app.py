7ff68ab86ac21fd1dd455cae10d03e42e76eff4d
diogomarin <70486076+diogomarin@users.noreply.github.com>
2024-08-27 22:00:27 -0300

    atualizando repositório remoto

--- myproject/app.py
+++ myproject/app.py
@@ -1,16 +1,37 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
 from models import db
+
+# Carrega variáveis de ambiente do .env se existirem
+load_dotenv()
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/mydatabase'
+# Configuração baseada no ambiente
+if 'WEBSITE_HOSTNAME' not in os.environ:
+    # Desenvolvimento local: usa variáveis de ambiente do arquivo .env
+    print("Carregando configuração de desenvolvimento.")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_DATABASE_URI')
+else:
+    # Produção
+    print("Carregando configuração de produção.")
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
+
+# Carregando a chave secreta do Flask para proteção de sessões e CSRF
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'defaultsecretkey')
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
+# Inicializa o banco de dados
 db.init_app(app)
+
+# Habilita comandos Flask-Migrate para migração do banco de dados
+migrate = Migrate(app, db)
 
 from routes import *
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()  # Cria as tabelas no banco de dados
     app.run(debug=True)
